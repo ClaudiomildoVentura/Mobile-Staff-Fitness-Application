6ade8e518eafeed7aa741b613fde9e452848e6cd
Claudiomildo <claudiomildo@gmail.com>
2019-12-12 20:34:00 -0300

    Adjust MainRoutes

--- components/Main/Settings/MainRoutes.js
+++ components/Main/Settings/MainRoutes.js
@@ -1,22 +1,19 @@
-import { createStackNavigator, createAppContainer }  from 'react-navigation';
+import { createStackNavigator, createAppContainer } from 'react-navigation'
 import MainHomeScreen from '../Pages/Home/MainHomeScreen'
 import StudentStackRoutes from '../../Student/Settings/StudentRoutes'
-//import MainShoppingStyles from '../Pages/MainShopping/MainShoppingScreen'
-//import StudentRegistrationScreen from '../../Student/Pages/StudentRegistration/StudentRegistrationScreen'
 import StudentMailActivationScreen from '../../Student/Pages/StudentActivation/StudentMailActivationScreen'
 import MainStudentRegistrationScreen from '../../Main/Pages/StudentRegistration/MainStudentRegistrationScreen'
+
 const MainRoutes = createStackNavigator({
   'Login': MainHomeScreen,
-//  'MainShopping': MainShoppingStyles,
   'Cadastro do Aluno': MainStudentRegistrationScreen,
   'Ativação do Email Aluno': StudentMailActivationScreen,
   'Rotas do Aluno': StudentStackRoutes
-}, 
-{
+},
+  {
     defaultNavigationOptions: {
-        header: null
+      header: null
     },
     initialRouteName: "Login"
-});
-
-export default createAppContainer(MainRoutes);
+  })
+export default createAppContainer(MainRoutes)
